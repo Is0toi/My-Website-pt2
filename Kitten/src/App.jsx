6ce275f13690938d78a0cedad8e299d47f9a1c9a
Is0toi <143647830+Is0toi@users.noreply.github.com>
2025-07-09 12:53:12 -0400

    Links work but unorganized

--- Kitten/src/App.jsx
+++ Kitten/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import Header from './Header.jsx';
+//import Header from './Header.jsx';
 import NavBar from './NavBar.jsx';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Project from './Project.jsx';
