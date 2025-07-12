3a40a54b2028075769d351feba721dfebb454744
Is0toi <143647830+Is0toi@users.noreply.github.com>
2025-07-12 00:26:30 -0400

    Hamburger Bar works!
    Works when clicked on

--- clock/src/component/NavBar.jsx
+++ clock/src/component/NavBar.jsx
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaBars } from "react-icons/fa6";
 import './NavBar.css'
 
 function NavBar() {
-  return <header>
+
+    const[isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+  return <>
+  <header>
     <div className = "container">
     <nav>
         <div className = "Title">
             <h2>Jessie's Website</h2>
         </div>
-        <ul className="nav-link">
+        <ul className={isOpen ? "nav-link active" : "nav-link"}>
             <li><a href = "/home"> Home</a></li>
             <li><a href = "/about"> About</a></li>
             <li><a href = "/contacts"> Contacts</a></li>
         </ul>
+        <div className="icon" onClick = {toggleMenu}>
+            <FaBars />
+            {/* this is our hamburger menu */}
+        </div>
     </nav>
   </div>
   </header>
+  <section>
+    <div className="container">
+        <div className="content">
+            <h2> hiiiii</h2>
+        </div>
+
+    </div>
+  </section>
+  </>
 }
 
 export default NavBar
