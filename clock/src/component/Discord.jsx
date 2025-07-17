5583c941e802a851d41e63cb65ec0aa51d8c8ebd
Is0toi <143647830+Is0toi@users.noreply.github.com>
2025-07-17 16:19:34 -0400

    fixed spotify button

--- clock/src/component/Discord.jsx
+++ clock/src/component/Discord.jsx
@@ -4,19 +4,19 @@ function Discord() {
     const [spotifyData, setSpotifyData] = useState(null);
 
     useEffect(() => {
-        const fetchStatus = async() => {
-            try{
+        const fetchStatus = async () => {
+            try {
                 const response = await fetch(`https://api.lanyard.rest/v1/users/693225286237552640`);
                 const json = await response.json();
-                if (json.success){
+                if (json.success) {
                     const spotify = json.data.listening_to_spotify ? json.data.spotify : null;
                     setSpotifyData(spotify);
                 }
-                else{
+                else {
                     setSpotifyData(null);
                 }
             }
-            catch(error){
+            catch (error) {
                 console.error("eRrOR: ", error);
                 setSpotifyData(null);
             }
@@ -27,18 +27,19 @@ function Discord() {
         return () => clearInterval(interval);
     }, []);
 
-    if(!spotifyData){
-        return <p style = {{ textAlign: "center" }}> Isotoi is not listening to Spotify right now!</p>;
+    if (!spotifyData) {
+        return <p style={{ textAlign: "center" }}> Isotoi is not listening to Spotify right now!</p>;
     }
 
     return (
-        <div class = "spotify">
-            <h2> Now playing on Spotify:</h2>
-            <p><strong> {spotifyData.song}</strong> by {spotifyData.artists}</p>
-            <img className = "album" src = {spotifyData.album_art_url} alt = "Album cover" />
-            <p>Album: {spotifyData.album}</p>
+        <div style={{ textAlign: "center" }}>
             <a href={`https://open.spotify.com/track/${spotifyData.track_id}`} target="_blank" rel="noopener noreferrer">
-                Listen On Spotify
+                <div class="spotify">
+                    <h2> Now playing on Spotify:</h2>
+                    <p><strong> {spotifyData.song}</strong> by {spotifyData.artists}</p>
+                    <img className="album" src={spotifyData.album_art_url} alt="Album cover" />
+                    <p>Album: {spotifyData.album}</p>
+                </div>
             </a>
         </div>
     );
