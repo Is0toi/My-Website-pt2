130eb8969cb6c900333d1fcc18fd060ed488b81e
Is0toi <143647830+Is0toi@users.noreply.github.com>
2025-07-17 17:35:08 -0400

    Transitions...
    Not the best but it's okay

--- clock/src/component/Projects.jsx
+++ clock/src/component/Projects.jsx
@@ -1,42 +1,62 @@
-import React from "react";
+import React,{useEffect} from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./NavBar";
 import './card.css'
 
 function Projects() {
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('animate');
+                }
+            });
+        }, {
+            threshold: 0.1
+        });
+
+        const cards = document.querySelectorAll('.card');
+        cards.forEach(card => observer.observe(card));
+
+        return () => {
+            cards.forEach(card => observer.unobserve(card));
+        };
+    }, []);
+
     return (
-        <div>
+        <div className="animation">
             <a className="link-font" href="https://www.youtube.com/watch?v=fB3SSmhBG-I" target="_blank" rel="noopener noreferrer">
-            <div className="card">
-                <img className="volleyball-icon" src='./volleyball-icon.jpg' alt="volleyball icon" style={{ width: '350px', height: '250px' }}></img>
-                <h2>Volleyball</h2>
-                <p>My biggest passion since I was 12</p>
-            </div>
+                <div className="card">
+                    <img className="volleyball-icon" src='./volleyball-icon.jpg' alt="volleyball icon" style={{ width: '350px', height: '250px' }}></img>
+                    <h2>Volleyball</h2>
+                    <p>My biggest passion since I was 12</p>
+                </div>
             </a>
 
             <a className="link-font" href="https://www.instagram.com/p/CnTSvfrtlLR/?img_index=1" target="_blank" rel="noopener noreferrer">
-            <div className="card">
-                <img className="robotics-icon" src='./robotics.jpg' alt="robotics icon" style={{ width: '350px', height: '233px' }}></img>
-                <h2>Robotics</h2>
-                <p> I love robotics, although busy from volleyball</p>
-                <p>it's a passion I want to pursue in the future</p>
-            </div>
+                <div className="card">
+                    <img className="robotics-icon" src='./robotics.jpg' alt="robotics icon" style={{ width: '350px', height: '233px' }}></img>
+                    <h2>Robotics</h2>
+                    <p> I love robotics, although busy from volleyball</p>
+                    <p>it's a passion I want to pursue in the future</p>
+                </div>
             </a>
 
             <a className="link-font" href="https://drive.google.com/file/d/1TpzDSIvcN9u-U2fgC50PfR5wuoHHlONP/view?usp=sharing" target="_blank" rel="noopener noreferrer">
-            <div className="card">
-                <img className="violinist-icon" src='./young-violinist.jpg' alt="violinist icon" style={{ width: '210px', height: '230px' }}></img>
-                <h2>Violin</h2>
-                <p> I've been playing violin since I was 7 and although I don't have time to do competitions I want to do some</p>
-            </div>
+                <div className="card">
+                    <img className="violinist-icon" src='./young-violinist.jpg' alt="violinist icon" style={{ width: '210px', height: '230px' }}></img>
+                    <h2>Violin</h2>
+                    <p> I've been playing violin since I was 7 and although I don't have time to do competitions I want to do some</p>
+                </div>
             </a>
 
             <a className="link-font" href="https://drive.google.com/file/d/1KMvqLMN-xhnzvPwtDf-fmGOECCKB6Pxc/view?usp=sharing" target="_blank" rel="noopener noreferrer">
-            <div className="card">
-                <img className="dance-icon" src='./dance.jpg' alt="dance icon" style={{ width: '250px', height: '210px' }}></img>
-                <h2>Dance</h2>
-                <p> I grew up doing dance, I've gotten out of my prime but I used to be able to have put my leg all the way to my head</p>
-            </div>
+                <div className="card">
+                    <img className="dance-icon" src='./dance.jpg' alt="dance icon" style={{ width: '250px', height: '210px' }}></img>
+                    <h2>Dance</h2>
+                    <p> I grew up doing dance, I've gotten out of my prime but I used to be able to have put my leg all the way to my head</p>
+                </div>
             </a>
 
             <a className="link-font" href="https://drive.google.com/drive/folders/1Oa1vfpYRJ8C8R1q91U7ICeeJE4_YPFjU?usp=sharing" target="_blank" rel="noopener noreferrer">
