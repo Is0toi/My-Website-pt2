679749327065108d7e63ceba02505761c65afbb5
Is0toi <143647830+Is0toi@users.noreply.github.com>
2025-07-17 21:47:08 -0400

    Grammar

--- clock/src/component/Home.jsx
+++ clock/src/component/Home.jsx
@@ -62,13 +62,13 @@ function Home() {
 
             <p style={{ textAlign: "center", fontSize: "1rem" }}>–––––––––––––––––––––––––––––––––––––––––––––</p>
             <div className="about-me-text">
-                <p > Hi! my name is Jessie Wang and I am 17 years old. I enjoy coding and volleyball. I currently live in Massachusetts. Volleyball is a passion and I've won multiple awards for it like being featured on the Boston Globe. </p>
+                <p > Hi! my name is Jessie Wang and I am 17 years old. I enjoy coding and volleyball. I currently live in Massachusetts. Volleyball is a passion of mine and I've won multiple awards for it like being featured on the Boston Globe. </p>
                 <p >Here are some fun facts about me:</p>
             </div>
             <ul className="funfacts">
                 <p>––––––––––––––––––––––––––––––––––</p>
-                <li> I am Canadian and haven been raised in Cananda for 9 years</li>
-                <li> I've traveled around the world like Florida, Georgia, California, and my favorite Illinois</li>
+                <li> I am Canadian and have been raised in Cananda for 9 years</li>
+                <li> I've traveled around the nation like Florida, Georgia, California, and my favorite Illinois</li>
                 <li> I am working on this website so that I can go to a hackathon in Boston with my boyyfriendddd!</li>
                 <li> I really like cats:</li>
             </ul>
